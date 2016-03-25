d2a00f5033a3dde815f3ea88551815f56e404666
Bryan Helmig <bryan@bryanhelmig.com>
2016-03-24 23:16:08 -0700

    upload command and various other build cleanups

--- zapier.js
+++ zapier.js
@@ -21,8 +21,29 @@ var CONFIG_LOCATION = process.env.ZAPIER_CONFIG_LOCATION || '~/.zapier-platform'
 var CURRENT_APP_FILE = process.env.ZAPIER_CURRENT_APP_FILE || '.zapier-current-app';
 // TODO: || is temp hack
 var PLATFORM_VERSION = process.env.ZAPIER_PLATFORM_VERSION || '3.0.0';
+var DEF_PATH = 'build/definition.json';
+var BUILD_PATH = 'build/build.zip';
 
-// utils
+var ART = '\
+                zzzzzzzz                \n\
+      zzz       zzzzzzzz       zzz      \n\
+    zzzzzzz     zzzzzzzz     zzzzzzz    \n\
+   zzzzzzzzzz   zzzzzzzz   zzzzzzzzzz   \n\
+      zzzzzzzzz zzzzzzzz zzzzzzzzz      \n\
+        zzzzzzzzzzzzzzzzzzzzzzzz        \n\
+          zzzzzzzzzzzzzzzzzzzz          \n\
+zzzzzzzzzzzzzzz          zzzzzzzzzzzzzzz\n\
+zzzzzzzzzzzzzzz          zzzzzzzzzzzzzzz\n\
+zzzzzzzzzzzzzzz          zzzzzzzzzzzzzzz\n\
+zzzzzzzzzzzzzzz          zzzzzzzzzzzzzzz\n\
+          zzzzzzzzzzzzzzzzzzzz          \n\
+        zzzzzzzzzzzzzzzzzzzzzzzz        \n\
+      zzzzzzzzz zzzzzzzz zzzzzzzzz      \n\
+   zzzzzzzzzz   zzzzzzzz   zzzzzzzzzz   \n\
+    zzzzzzz     zzzzzzzz     zzzzzzz    \n\
+      zzz       zzzzzzzz       zzz      \n\
+                zzzzzzzz                ';
+
 
 // Wraps the easy-table library. Rows is an array of objects,
 // columnDefs an ordered sub-array [[label, key], ...].
@@ -179,7 +200,7 @@ var makeZip = function(dir, zipPath) {
       {
         expand: true,
         cwd: dir,
-        src: '**/*'
+        src: '**/*' // could do browserify --list
       }
     ]);
     archive.finalize();
@@ -224,11 +245,16 @@ var callAPI = function(route, options) {
 };
 
 // Reads the JSON file at ~/.zapier-platform (CONFIG_LOCATION).
-var getCurrentApp = function() {
+var getCurrentAppConfig = function() {
   return readFile(CURRENT_APP_FILE)
     .then(function(buf) {
       return JSON.parse(buf.toString()).id;
-    })
+    });
+};
+
+// Loads the current app from the API.
+var getCurrentApp = function() {
+  return getCurrentAppConfig()
     .then(function(appId) {
       return callAPI('/apps/' + appId);
     });
@@ -288,6 +314,103 @@ var listHistory = function() {
   return listEndoint('history');
 };
 
+var build = function(zipPath) {
+  var wdir = process.cwd();
+  zipPath = zipPath || BUILD_PATH;
+  var tmpDir = path.join(os.tmpdir(), 'zapier-' + crypto.randomBytes(4).toString('hex'));
+  return ensureDir(tmpDir)
+    .then(function() {
+      printStarting('  Copying project to temp directory');
+      return copyDir(wdir, tmpDir);
+    })
+    .then(function() {
+      printDone();
+      printStarting('  Installing project dependencies');
+      return runCommand('npm install --production', {cwd: tmpDir});
+    })
+    // tries to make a more reproducible zip build!
+    .then(function() {
+      // https://blog.pivotal.io/labs/labs/barriers-deterministic-reproducible-zip-files
+      // https://reproducible-builds.org/tools/ or strip-nondeterminism
+      return runCommand('find . -exec touch -t 201601010000 {} +', {cwd: tmpDir});
+      // the next two break require('') if omitted :-/
+      // if we browserify --list the next two can drop
+      // .then(function() {
+      //   // npm package.json has weird _args and _shasum style stuff
+      //   return runCommand('find node_modules -name "package.json" -delete', {cwd: tmpDir});
+      // })
+      // .then(function() {
+      //   // Makefile self edits
+      //   return runCommand('find node_modules -name "Makefile" -delete', {cwd: tmpDir});
+      // });
+    })
+    .then(function() {
+      printDone();
+      printStarting('  Building app definition (TODO!)');
+      return Promise.resolve('TODO!');
+    })
+    .then(function() {
+      printDone();
+      printStarting('  Zipping project and dependencies');
+      return makeZip(tmpDir, wdir + '/' + zipPath);
+    })
+    .then(function() {
+      printDone();
+      printStarting('  Cleaning up temp directory');
+      return removeDir(tmpDir);
+    })
+    .then(function() {
+      printDone();
+      return zipPath;
+    });
+};
+
+var upload = function(zipPath, defPath) {
+  zipPath = zipPath || BUILD_PATH;
+  defPath = defPath || DEF_PATH;
+  return getCurrentApp()
+    .then(function(app) {
+      var definition = readFile(defPath)
+        .then(function(buf) {
+          return JSON.parse(buf.toString());
+        });
+      var zipFile = readFile(zipPath)
+        .then(function(buf) {
+          return buf.toString('base64');
+        });
+      return Promise.all([definition, zipFile, app]);
+    })
+    .then(function(values) {
+      var definition = values[0];
+      var zipFile = values[1];
+      var app = values[2];
+
+      printStarting('  Uploading version ' + definition.version);
+      return callAPI('/apps/' + app.id + '/versions/' + definition.version, {
+        method: 'PUT',
+        body: {
+          platform_version: PLATFORM_VERSION || definition.platformVersion,
+          definition: definition,
+          zip_file: zipFile
+        }
+      });
+    })
+    .then(function() {
+      printDone();
+    });
+};
+
+var buildAndUploadCurrentDir = function(zipPath) {
+  zipPath = zipPath || BUILD_PATH;
+  return checkCredentials()
+    .then(function() {
+      return build(zipPath);
+    })
+    .then(function() {
+      return upload(zipPath);
+    });
+};
+
 
 // commands
 
@@ -332,10 +455,24 @@ configCmd.example = 'zapier config';
 var createCmd = function(title) {
   return checkCredentials()
     .then(function() {
+      console.log('Welcome to the Zapier Platform! :-D');
+      console.log();
+      console.log(ART);
+      console.log();
+      console.log('Let\'s create your app "' + title + '"!');
+      console.log();
       printStarting('  Cloning starter app from ' + STARTER_REPO);
       // var cmd = 'git clone https://github.com/' + STARTER_REPO + '.git .';
       var cmd = 'git clone git@github.com:' + STARTER_REPO + '.git .';
       return runCommand(cmd);
+    })
+    .then(function() {
+      return removeDir('.git');
+    })
+    .then(function() {
+      printDone();
+      printStarting('  Installing project dependencies');
+      return runCommand('npm install');
     })
     .then(function() {
       printDone();
@@ -355,12 +492,9 @@ var createCmd = function(title) {
         key: app.key
       }));
     })
-    .then(function(results) {
-      console.log(results);
-      return removeDir('.git');
-    })
     .then(function() {
       printDone();
+      console.log('\nFinished! You can `zapier push` now - or make tweaks!');
     });
 };
 createCmd.docs = 'Creates a new app in your account.';
@@ -381,41 +515,11 @@ var appsCmd = function() {
 appsCmd.docs = 'Lists all the apps in your account.';
 appsCmd.example = 'zapier apps';
 
-var buildCmd = function() {
-  var wdir = process.cwd();
-  var tmpDir = path.join(os.tmpdir(), 'zapier-' + crypto.randomBytes(4).toString('hex'));
+var buildCmd = function(zipPath) {
   console.log('Building project.\n');
-  return ensureDir(tmpDir)
-    .then(function() {
-      printStarting('  Copying project to temp directory');
-      return copyDir(wdir, tmpDir);
-    })
-    .then(function() {
-      printDone();
-      printStarting('  Installing project dependencies');
-      return runCommand('npm install --production', {cwd: tmpDir});
-    })
-    .then(function() {
-      // tries to make a more reproducible zip build
-      // https://blog.pivotal.io/labs/labs/barriers-deterministic-reproducible-zip-files
-      // https://reproducible-builds.org/tools/ or strip-nondeterminism
-      return runCommand('find . -exec touch -t 201601010000 {} +', {cwd: tmpDir});
-    })
-    .then(function() {
-      printDone();
-      printStarting('  Zipping project and dependencies');
-      return makeZip(tmpDir, wdir + '/build.zip');
-    })
-    .then(function() {
-      printDone();
-      printStarting('  Cleaning up temp directory');
-      return removeDir(tmpDir);
-    })
-    .then(function() {
-      printDone();
-    });
+  return build(zipPath);
 };
-buildCmd.docs = 'Builds a deployable zip from the current directory. (Debug only)';
+buildCmd.docs = 'Builds a deployable zip from the current directory.';
 buildCmd.example = 'zapier build';
 
 var versionsCmd = function() {
@@ -435,41 +539,26 @@ versionsCmd.docs = 'Lists all the versions of the current app.';
 versionsCmd.example = 'zapier versions';
 
 var pushCmd = function() {
-  return checkCredentials()
-    .then(getCurrentApp)
-    .then(function(app) {
-      var definition = readFile('app-definition.json')
-        .then(function(buf) {
-          return JSON.parse(buf.toString());
-        });
-      var zipFile = readFile('build.zip')
-        .then(function(buf) {
-          return buf.toString('base64');
-        });
-      return Promise.all([definition, zipFile, app]);
-    })
-    .then(function(values) {
-      var definition = values[0];
-      var zipFile = values[1];
-      var app = values[2];
-      console.log('Preparing to push version ' + definition.version + ' of your app "' + app.title + '".\n');
-
-      printStarting('  Pushing version ' + definition.version);
-      return callAPI('/apps/' + app.id + '/versions/' + definition.version, {
-        method: 'PUT',
-        body: {
-          platform_version: PLATFORM_VERSION || definition.platformVersion,
-          definition: definition,
-          zip_file: zipFile
-        }
-      });
-    })
+  var zipPath = zipPath || BUILD_PATH;
+  console.log('Preparing to build and upload a new version.\n');
+  return buildAndUploadCurrentDir(zipPath)
     .then(function() {
-      printDone();
+      console.log('\nBuild and upload complete!');
     });
 };
-pushCmd.docs = 'Push a new version of the current app - does not deploy.';
-pushCmd.example = 'zapier push 1.0.0';
+pushCmd.docs = 'Build and upload a new version of the current app - does not deploy.';
+pushCmd.example = 'zapier push';
+
+var uploadCmd = function() {
+  var zipPath = zipPath || BUILD_PATH;
+  console.log('Preparing to upload a new version.\n');
+  return upload(zipPath)
+    .then(function() {
+      console.log('\nUpload complete!');
+    });
+};
+uploadCmd.docs = 'Just upload the last build - does not deploy.';
+uploadCmd.example = 'zapier upload';
 
 var deploymentsCmd = function() {
   return listDeployments()
@@ -534,6 +623,7 @@ commands = {
   build: buildCmd, // debug only?
   apps: appsCmd,
   push: pushCmd,
+  upload: uploadCmd, // debug only?
   versions: versionsCmd,
   deploy: deployCmd,
   deployments: deploymentsCmd,
@@ -565,6 +655,7 @@ var main = function(argv) {
       console.log('');
     })
     .catch(function(err) {
+      console.log('\n');
       console.log('Error ' + err.message);
       console.log('\nFailed!');
       throw err;
