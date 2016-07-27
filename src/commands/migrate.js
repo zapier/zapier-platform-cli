9e3932eb44093ea1c44f280a87db8fce337f4b31
Bryan Helmig <bryan@bryanhelmig.com>
2016-07-27 15:32:19 -0700

    migrate fleshed out

--- src/commands/migrate.js
+++ src/commands/migrate.js
@@ -5,12 +5,12 @@ var migrateCmd = (oldVersion, newVersion, optionalPercent = '100%') => {
     console.log('Must provide both old and new version like `zapier migrate 1.0.0 1.0.1`.');
     return Promise.resolve();
   }
-  optionalPercent = parseInt(optionalPercent, 10) / 100.0;
+  optionalPercent = parseInt(optionalPercent, 10);
   return utils.getLinkedApp()
     .then(app => {
       console.log(`Getting ready to migrate your app ${app.title} from ${oldVersion} to ${newVersion}.\n`);
       // r'^apps/(?P<app_id>\d+)/versions/(?P<version_from>\d{1,3}\.\d{1,3}\.\d{1,3})/migrate-to/(?P<version_to>\d{1,3}\.\d{1,3}\.\d{1,3})$',
-      utils.printStarting(`Migrating ${oldVersion} to ${newVersion}`);
+      utils.printStarting(`Migrating ${oldVersion} to ${newVersion} for ${optionalPercent}%`);
       return utils.callAPI(`/apps/${app.id}/versions/${oldVersion}/migrate-to/${newVersion}`, {
         method: 'POST',
         body: {
@@ -18,8 +18,9 @@ var migrateCmd = (oldVersion, newVersion, optionalPercent = '100%') => {
         }
       });
     })
-    .then(resp => {
+    .then(() => {
       utils.printDone();
+      console.log('\nDeploy successfully queued, please check `zapier history` to track the status. Normal deploys take between 5-10 minutes.');
     });
 };
 migrateCmd.help = 'Migrate users from one version to another.';
