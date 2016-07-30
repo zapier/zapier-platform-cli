bdf49ae911ae4c0af53ff4a4a6a0bb0657923a5b
Bryan Helmig <bryan@bryanhelmig.com>
2016-07-30 16:28:56 -0700

    use litdoc instead

--- src/bin/docs.js
+++ src/bin/docs.js
@@ -2,95 +2,13 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 
 const _ = require('lodash');
 
-const marked = require('marked');
-const toc = require('markdown-toc');
-const cheerio = require('cheerio');
-const hljs = require('highlight.js');
+const litdoc = require('litdoc');
 
 const commands = require('../commands');
-
-const makeTemplateRenderer = (t) => _.template(t, {interpolate: /<%=([\s\S]+?)%>/g});
-
-// const oldLex = marked.Lexer.lex;
-// marked.Lexer.lex = function() {
-//   const out = oldLex.apply(this, arguments);
-//   out.forEach(lexed => {
-//     console.log(lexed);
-//     console.log('-----');
-//   });
-//   return out;
-// };
-
-const renderMarkdownString = (markdownString) => {
-  const rowTemplate = makeTemplateRenderer(`\
-  <div class="row">
-    <div class="row-height">
-      <div class="col-md-6 col-height docs-primary">
-        <%= left %>
-      </div>
-      <div class="col-md-6 col-height docs-code">
-        <%= right %>
-      </div>
-    </div>
-  </div>`);
-
-  const row = (left, right) => rowTemplate({left: left || '', right: right || ''});
-
-  const intermediaryOutput = marked(markdownString, {
-    highlight: (code, lang) => {
-      if (!lang || lang === 'plain') { return code; }
-      return hljs.highlight(lang, code).value;
-    }
-  });
-
-  const $ = cheerio.load(`<div id="root">${intermediaryOutput}</div>`);
-  const blocks = [
-    'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ul',
-    'li', 'blockquote', 'table', 'hr', 'br', 'pre'
-  ];
-  const collectLeft = ['p', 'blockquote'];
-  const flushRight = ['pre'];
-
-  let stack = [];
-  let finalOutput = '';
-  // walk all the "root" block level elements, throw anything that can
-  // be bunched into the left side of code onto the stack - then flush
-  // periodically when we run into code.
-  $(`#root > ${blocks.join(',')}`).each((i, el) => {
-    const collect = collectLeft.indexOf(el.name) !== -1;
-    const flush = flushRight.indexOf(el.name) !== -1;
-    let inner = $(el).clone().wrap('<div>').parent().html();
-
-    if (collect) {
-      stack.push(inner);
-      return;
-    }
-
-    if (flush) {
-      finalOutput += row(stack.join(''), inner);
-      stack = [];
-    } else {
-      if (stack.length) {
-        finalOutput += row(stack.join(''));
-      }
-      finalOutput += row(inner);
-    }
-
-    if (!collect) {
-      stack = [];
-    }
-
-  });
-
-  if (stack.length) {
-    finalOutput += row(stack.join(''));
-  }
-
-  return finalOutput;
-};
 
 
 const rollUpCli = () => {
@@ -109,24 +27,16 @@ const rollUpCli = () => {
   fs.writeFileSync('./docs/cli.md', docs);
 };
 
-
-const mdToHtml = () => {
-  const templateString = fs.readFileSync('./docs/index.template.html').toString();
-  const renderTemplate = makeTemplateRenderer(templateString);
-
-  const markdownString = fs.readFileSync('./docs/index.md').toString();
-  const tableOfContent = marked(toc(markdownString).content);
-
-  const content = renderMarkdownString(markdownString);
-  const context = {
-    tableOfContent: tableOfContent,
-    content: content
-  };
-
-  const finalContent = renderTemplate(context);
-  fs.writeFileSync('./docs/index.html', finalContent);
-};
-
-
 rollUpCli();
-mdToHtml();
+
+litdoc({
+  title: 'Zapier Platform CLI',
+  markdownPath: path.join(__dirname, '../../docs/index.md'),
+  outputPath: path.join(__dirname, '../../docs/index.html')
+});
+
+litdoc({
+  title: 'Zapier Platform CLI',
+  markdownPath: path.join(__dirname, '../../docs/cli.md'),
+  outputPath: path.join(__dirname, '../../docs/cli.html')
+});
