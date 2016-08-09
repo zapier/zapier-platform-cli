94e561807aaa79465fdb7d26858c3b463dee7823
stevemolitor <stevemolitor@gmail.com>
2016-08-09 18:09:27 -0500

    simplify utils/index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,89 +1,10 @@
-const {
-  writeFile,
-  readFile,
-  copyDir,
-  ensureDir,
-  removeDir,
-} = require('./files');
+const _ = require('lodash');
 
-const {
-  markdownLog,
-  makeTable,
-  printData,
-  prettyJSONstringify,
-  clearSpinner,
-  printStarting,
-  printDone,
-  getInput,
-} = require('./display');
-
-const {
-  readCredentials,
-  callAPI,
-  writeLinkedAppConfig,
-  getLinkedApp,
-  checkCredentials,
-  listApps,
-  listEndoint,
-  listVersions,
-  listHistory,
-  listCollaborators,
-  listInvitees,
-  listLogs,
-  listEnv,
-  upload,
-} = require('./api');
-
-const {
-  argParse,
-  camelCase,
-  snakeCase,
-  makePromise,
-  runCommand,
-  localAppCommand,
-  isValidNodeVersion
-} = require('./misc');
-
-const {
-  build,
-  buildAndUploadCurrentDir,
-} = require('./build');
-
-module.exports = {
-  markdownLog,
-  makeTable,
-  printData,
-  argParse,
-  prettyJSONstringify,
-  clearSpinner,
-  printStarting,
-  printDone,
-  makePromise,
-  getInput,
-  writeFile,
-  readFile,
-  copyDir,
-  ensureDir,
-  removeDir,
-  readCredentials,
-  runCommand,
-  localAppCommand,
-  isValidNodeVersion,
-  callAPI,
-  writeLinkedAppConfig,
-  getLinkedApp,
-  checkCredentials,
-  listApps,
-  listEndoint,
-  listVersions,
-  listHistory,
-  listCollaborators,
-  listInvitees,
-  listLogs,
-  listEnv,
-  build,
-  upload,
-  buildAndUploadCurrentDir,
-  camelCase,
-  snakeCase,
-};
+module.exports = _.extend(
+  {},
+  require('./files'),
+  require('./display'),
+  require('./api'),
+  require('./misc'),
+  require('./build')
+);
