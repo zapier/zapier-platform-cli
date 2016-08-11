d5a5e23247b51a6a1c2da8ebe8b8c9aff40d6861
Bryan Helmig <bryan@bryanhelmig.com>
2016-08-10 17:18:35 -0700

    migrate command docs

--- src/commands/migrate.js
+++ src/commands/migrate.js
@@ -8,9 +8,8 @@ var migrate = (oldVersion, newVersion, optionalPercent = '100%') => {
   optionalPercent = parseInt(optionalPercent, 10);
   return utils.getLinkedApp()
     .then(app => {
-      console.log(`Getting ready to migrate your app ${app.title} from ${oldVersion} to ${newVersion}.\n`);
-      // r'^apps/(?P<app_id>\d+)/versions/(?P<version_from>\d{1,3}\.\d{1,3}\.\d{1,3})/migrate-to/(?P<version_to>\d{1,3}\.\d{1,3}\.\d{1,3})$',
-      utils.printStarting(`Migrating ${oldVersion} to ${newVersion} for ${optionalPercent}%`);
+      console.log(`Getting ready to migrate your app "${app.title}" from ${oldVersion} to ${newVersion}.\n`);
+      utils.printStarting(`Starting migration from ${oldVersion} to ${newVersion} for ${optionalPercent}%`);
       return utils.callAPI(`/apps/${app.id}/versions/${oldVersion}/migrate-to/${newVersion}`, {
         method: 'POST',
         body: {
@@ -26,9 +25,27 @@ var migrate = (oldVersion, newVersion, optionalPercent = '100%') => {
 migrate.help = 'Migrate users from one version to another.';
 migrate.example = 'zapier migrate 1.0.0 1.0.1 [10%]';
 migrate.docs = `\
-**TODO!**
+Starts a migration to move users between different versions of your app. You may also "revert" by simply swapping the from/to verion strings in the command line arguments (IE: \`zapier migrate 1.0.1 1.0.0\`).
 
-This is markdown documentation.
+Migrations can take between 5-10 minutes, so be patient and check \`zapier history\` to track the status.
+
+> Tip! We recommend migrating a small subset of users first, then watching error logs for the new version for any sort of odd behavior. When you feel confident there are no bugs, go ahead and migrate everyone. If you see unexpected errors, you can revert simply.
+
+**Options**
+
+* \`1.0.0\` -- the version **from** which to migrate users
+* \`1.0.1\` -- the version **to** which to migrate users
+* \`[10%]\` -- an optional percent of users to migrate, default is \`100%\`
+${utils.defaultOptionsDocFragment({cmd: 'versions'})}
+
+${'```'}bash
+$ zapier migrate 1.0.0 1.0.1 15%
+# Getting ready to migrate your app "Example" from 1.0.0 to 1.0.1.
+# 
+#   Starting migration from 1.0.0 to 1.0.1 for 15% - done!
+# 
+# Deploy successfully queued, please check \`zapier history\` to track the status. Normal deploys take between 5-10 minutes.
+${'```'}
 `;
 
 module.exports = migrate;
