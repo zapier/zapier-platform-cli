9a5f37ae06f662aa8565b1b9ec4452efa9bf51bc
Bryan Helmig <bryan@bryanhelmig.com>
2016-08-12 15:05:07 -0700

    add index scaffold

---
+++ scaffold/index.template.js
@@ -0,0 +1,23 @@
+const App = {
+  version: require('./package.json').version,
+  platformVersion: require('./package.json').dependencies['@zapier/zapier-platform-core'],
+
+  models: {
+
+  },
+
+  triggers: {
+
+  },
+
+  searches: {
+
+  },
+
+  writes: {
+
+  }
+
+};
+
+module.exports = App;
