9862b3579b207a359a5ab903ad06afa64450ce72
Brian Cooksey <bac2k@hotmail.com>
2016-08-23 17:42:44 -0500

    Update template app to reference resource instead of model

--- templates/default-app/test/resources.js
+++ templates/default-app/test/resources.js
@@ -3,6 +3,6 @@ const should = require('should');
 const zapier = require('@zapier/zapier-platform-core');
 const runApp = zapier.exposeAppTestable(require('../index'));
 
-describe('models', () => {
+describe('resources', () => {
 
 });
