8626c40f03cc9b492f10700633d790ec0f066809
David Brownman <beamneocube@gmail.com>
2017-04-14 22:13:58 -0700

    hit server for style check and display them

--- src/commands/validate.js
+++ src/commands/validate.js
@@ -5,7 +5,7 @@ const utils = require('../utils');
 
 
 const validate = (context) => {
-  context.line('Validating project locally.\n');
+  context.line('\nValidating project locally.');
   return Promise.resolve()
     .then(() => utils.localAppCommand({command: 'validate'}))
     .then((errors) => {
@@ -15,7 +15,7 @@ const validate = (context) => {
         error.docLinks = (error.docLinks || []).join('\n');
         return error;
       });
-      const ifEmpty = colors.grey('No errors found during validation routine.');
+      const ifEmpty = colors.grey('No structural errors found during validation routine.');
       utils.printData(newErrors, [
         ['Property', 'property'],
         ['Message', 'message'],
@@ -25,14 +25,56 @@ const validate = (context) => {
     })
     .then((errors) => {
       if (errors.length) {
-        context.line(`\nMake any changes to your project and rerun this command.`);
+        context.line('Your app is structurally invalid. Address concerns and run this command again.');
       } else {
-        context.line(`\nThis project looks good!`);
+        context.line('This project looks good!');
+      }
+    })
+    .then(() => {
+      if (global.argOpts['include-style']) {
+        utils.localAppCommand({ command: 'definition' })
+          .then((rawDefinition) => {
+            return utils.callAPI('/style-check', {
+              skipDeployKey: true,
+              method: 'POST',
+              body: rawDefinition
+            });
+          })
+          .then((styleResult) => {
+            // process errors
+            let res = [];
+            context.line('\nChecking app style.');
+            const ifEmpty = colors.grey('No style errors found during validation routine.');
+            for (const severity in styleResult) {
+              for (const type in styleResult[severity]) {
+                for (const method in styleResult[severity][type]) {
+                  res.push({
+                    category: severity,
+                    method: `${type}.${method}`,
+                    description: styleResult[severity][type][method].join('\n')
+                  });
+                }
+              }
+            }
+
+            if (res.length) {
+              utils.printData(res, [
+                ['Category', 'category'],
+                ['Method', 'method'],
+                ['Description', 'description'],
+              ], ifEmpty, true);
+              context.line('Errors will prevent deploys, warnings are things to improve on.\n');
+            } else {
+              context.line('Your app looks great!\n');
+            }
+          });
       }
     });
 };
 validate.argsSpec = [];
-validate.argOptsSpec = {};
+validate.argOptsSpec = {
+  'include-style': { flag: true, help: 'ping the Zapier server to do a style check' },
+};
 validate.help = 'Validates the current app.';
 validate.example = 'zapier validate';
 validate.docs = `\
@@ -47,21 +89,21 @@ ${utils.defaultArgOptsFragment()}
 ${'```'}bash
 $ zapier validate
 # Validating project locally.
-# 
+#
 # No errors found during validation routine.
-# 
+#
 # This project looks good!
 
 $ zapier validate
 # Validating project locally.
-# 
+#
 # ┌────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
 # │ = 1 =                                                                                                      │
 # │     Property │ instance                                                                                    │
 # │     Message  │ requires property "platformVersion"                                                         │
 # │     Links    │ https://github.com/zapier/zapier-platform-schema/blob/v1.0.0/docs/build/schema.md#appschema │
 # └──────────────┴─────────────────────────────────────────────────────────────────────────────────────────────┘
-# 
+#
 # Make any changes to your project and rerun this command.
 ${'```'}
 `;
