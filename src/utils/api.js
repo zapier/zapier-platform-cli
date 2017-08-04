2053e7373f57cb31e73a20dd0d4b6656dc7aeb68
Bruno Bernardino <bruno@bernardino.io>
2017-08-04 10:03:08 +0100

    Adding upgrade docs

--- src/utils/api.js
+++ src/utils/api.js
@@ -271,7 +271,7 @@ const upload = (zipPath, appDir) => {
       printDone();
 
       if (semver.lt(appVersion.platform_version, appVersion.core_npm_version)) {
-        console.log(`\n**NOTE:** Your app is using zapier-platform-core@${appVersion.platform_version}, and there's a new version: ${appVersion.core_npm_version}. Please consider updating it: https://zapier.com/developer/documentation/v2/upgrading-zapier-platform-cli-or-zapier-platform-core/`);
+        console.log(`\n**NOTE:** Your app is using zapier-platform-core@${appVersion.platform_version}, and there's a new version: ${appVersion.core_npm_version}. Please consider updating it: https://zapier.github.io/zapier-platform-cli/#upgrading-zapier-platform-cli-or-zapier-platform-core`);
       }
     });
 };
