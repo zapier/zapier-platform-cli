c01a18c9e3b9cad2c7edd34b62462be2b27fb6d8
Chang-Hung Liang <chang-hung.liang@zapier.com>
2017-11-16 19:17:05 +0800

    zapier convert: Generate different search code based on scripting

--- scaffold/convert/search.template.js
+++ scaffold/convert/search.template.js
@@ -1,12 +1,106 @@
 // Search stub created by 'zapier convert'. This is just a stub - you will need to edit!
+<%
+// Template for just _pre_search()
+if (scripting && preScripting && !postScripting && !fullScripting) { %>
+const getList = (z, bundle) => {
+  const scripting = require('../scripting');
+  const legacyScriptingRunner = require('zapier-platform-legacy-scripting-runner')(scripting);
 
+  bundle._legacyUrl = '<%= URL %>';
+
+  // Do a _pre_search() from scripting.
+  const preSearchEvent = {
+    name: 'search.pre',
+    key: '<%= KEY %>'
+  };
+  return legacyScriptingRunner.runEvent(preSearchEvent, z, bundle)
+    .then((preSearchResult) => z.request(preSearchResult))
+    .then((response) => z.JSON.parse(response.content));
+};
+<%
+}
+
+// Template for _pre_search() + _post_search()
+if (scripting && preScripting && postScripting && !fullScripting) { %>
+const getList = (z, bundle) => {
+  const scripting = require('../scripting');
+  const legacyScriptingRunner = require('zapier-platform-legacy-scripting-runner')(scripting);
+
+  bundle._legacyUrl = '<%= URL %>';
+
+  // Do a _pre_search() from scripting.
+  const preSearchEvent = {
+    name: 'search.pre',
+    key: '<%= KEY %>'
+  };
+  return legacyScriptingRunner.runEvent(preSearchEvent, z, bundle)
+    .then((preSearchResult) => z.request(preSearchResult))
+    .then((response) => {
+      // Do a _post_search() from scripting.
+      const postSearchEvent = {
+        name: 'search.post',
+        key: '<%= KEY %>',
+        response
+      };
+      return legacyScriptingRunner.runEvent(postSearchEvent, z, bundle);
+    });
+};
+<%
+}
+
+// Template for just _post_search()
+if (scripting && !preScripting && postScripting) { %>
+const getList = (z, bundle) => {
+  const scripting = require('../scripting');
+  const legacyScriptingRunner = require('zapier-platform-legacy-scripting-runner')(scripting);
+
+  bundle._legacyUrl = '<%= URL %>';
+
+  const responsePromise = z.request({
+    url: bundle._legacyUrl
+  });
+  return responsePromise
+    .then((response) => {
+      // Do a _post_search() from scripting.
+      const postSearchEvent = {
+        name: 'search.post',
+        key: '<%= KEY %>',
+        response
+      };
+      return legacyScriptingRunner.runEvent(postSearchEvent, z, bundle);
+    });
+};
+<%
+}
+
+// Template for just _search()
+if (scripting && fullScripting) { %>
+const getList = (z, bundle) => {
+  const scripting = require('../scripting');
+  const legacyScriptingRunner = require('zapier-platform-legacy-scripting-runner')(scripting);
+
+  bundle._legacyUrl = '<%= URL %>';
+
+  // Do a _search() from scripting.
+  const fullSearchEvent = {
+    name: 'search.search',
+    key: '<%= KEY %>',
+  };
+  return legacyScriptingRunner.runEvent(fullSearchEvent, z, bundle);
+};
+<%
+}
+
+// If there's no scripting, it's even sweeter and simpler!
+if (!scripting) { %>
 const getList = (z, bundle) => {
   const responsePromise = z.request({
     url: '<%= URL %>'
   });
   return responsePromise
-    .then(response => JSON.parse(response.content));
+    .then(response => z.JSON.parse(response.content));
 };
+<% } %>
 
 module.exports = {
   key: '<%= KEY %>',
