7a1463215598955d3cec9ac3998d535fee201a52
Bruno Bernardino <bruno@bernardino.io>
2017-11-28 16:00:59 +0000

    Prevent using a percentage other than 100% and user.
    Also tweak description of `zapier env` to make it clearer it's possible to delete vars. (for https://trello.com/c/dicfOUIw/176-allow-cli-environment-variables-to-be-deleted)
    
    Related to #192

--- src/commands/migrate.js
+++ src/commands/migrate.js
@@ -14,7 +14,11 @@ const migrate = (context, fromVersion, toVersion, optionalPercent = '100%') => {
       const user = global.argOpts.user;
 
       if (user) {
-        body.percent = 100;
+        if (optionalPercent !== 100) {
+          throw new Error('Cannot define percent and user. Use only one or the other.');
+        }
+
+        body.percent = optionalPercent;
         body.user = user;
 
         context.line(`Getting ready to migrate "${user}" in your app "${app.title}" from ${fromVersion} to ${toVersion}.\n`);
