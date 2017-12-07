b73b2c6b217931a41e9e828484bfc03203cf43b7
Chang-Hung Liang <chang-hung.liang@zapier.com>
2017-12-07 21:33:16 +0800

    zapier convert: Add custom output field support to searches

--- scaffold/convert/search.template.js
+++ scaffold/convert/search.template.js
@@ -878,8 +878,99 @@ const getInputFields = (z, bundle) => {
   return responsePromise
     .then((response) => z.JSON.parse(response.content));
 };
-<% } %>
+<% }
 
+if (outputFieldFullScripting) {%>
+const getOutputFields = (z, bundle) => {
+  const scripting = require('../scripting');
+  const legacyScriptingRunner = require('zapier-platform-legacy-scripting-runner')(scripting);
+
+  bundle._legacyUrl = '<%= CUSTOM_FIELDS_RESULT_URL %>';
+  bundle._legacyUrl = legacyScriptingRunner.replaceVars(bundle._legacyUrl, bundle, {});
+
+  // Do a _custom_search_fields() from scripting.
+  const fullResultFieldsEvent = {
+    name: 'search.output',
+    key: '<%= KEY %>',
+  };
+  return legacyScriptingRunner.runEvent(fullResultFieldsEvent, z, bundle);
+};
+<% } else if (outputFieldPreScripting && !outputFieldPostScripting) { %>
+const getOutputFields = (z, bundle) => {
+  const scripting = require('../scripting');
+  const legacyScriptingRunner = require('zapier-platform-legacy-scripting-runner')(scripting);
+
+  bundle._legacyUrl = '<%= CUSTOM_FIELDS_RESULT_URL %>';
+  bundle._legacyUrl = legacyScriptingRunner.replaceVars(bundle._legacyUrl, bundle, {});
+
+  // Do a _pre_custom_search_fields() from scripting.
+  const preResultFieldsEvent = {
+    name: 'search.output.pre',
+    key: '<%= KEY %>',
+  };
+  return legacyScriptingRunner.runEvent(preResultFieldsEvent, z, bundle)
+    .then((preResultFieldsResult) => z.request(preResultFieldsResult))
+    .then((response) => z.JSON.parse(response.content));
+};
+<% } else if (outputFieldPreScripting && outputFieldPostScripting) { %>
+const getOutputFields = (z, bundle) => {
+  const scripting = require('../scripting');
+  const legacyScriptingRunner = require('zapier-platform-legacy-scripting-runner')(scripting);
+
+  bundle._legacyUrl = '<%= CUSTOM_FIELDS_RESULT_URL %>';
+  bundle._legacyUrl = legacyScriptingRunner.replaceVars(bundle._legacyUrl, bundle, {});
+
+  // Do a _pre_custom_search_fields() from scripting.
+  const preResultFieldsEvent = {
+    name: 'search.output.pre',
+    key: '<%= KEY %>',
+  };
+  return legacyScriptingRunner.runEvent(preResultFieldsEvent, z, bundle)
+    .then((preResultFieldsResult) => z.request(preResultFieldsResult))
+    .then((response) => {
+      // Do a _post_custom_search_fields() from scripting.
+      const postResultFieldsEvent = {
+        name: 'search.output.post',
+        key: '<%= KEY %>',
+        response,
+      };
+      return legacyScriptingRunner.runEvent(postResultFieldsEvent, z, bundle);
+    });
+};
+<% } else if (!outputFieldPreScripting && outputFieldPostScripting) { %>
+const getOutputFields = (z, bundle) => {
+  const scripting = require('../scripting');
+  const legacyScriptingRunner = require('zapier-platform-legacy-scripting-runner')(scripting);
+
+  bundle._legacyUrl = '<%= CUSTOM_FIELDS_RESULT_URL %>';
+  bundle._legacyUrl = legacyScriptingRunner.replaceVars(bundle._legacyUrl, bundle, {});
+
+  const responsePromise = z.request({
+    url: bundle._legacyUrl
+  });
+  return responsePromise
+    .then((response) => {
+      // Do a _post_custom_search_fields() from scripting.
+      const postResultFieldsEvent = {
+        name: 'search.output.post',
+        key: '<%= KEY %>',
+        response,
+      };
+      return legacyScriptingRunner.runEvent(postResultFieldsEvent, z, bundle);
+    });
+};
+<% } else if (hasCustomOutputFields) { %>
+const getOutputFields = (z, bundle) => {
+  let url = '<%= CUSTOM_FIELDS_RESULT_URL %>';
+  url = replaceVars(url, bundle, {});
+
+  const responsePromise = z.request({
+    url: url
+  });
+  return responsePromise
+    .then((response) => z.JSON.parse(response.content));
+};
+<% } %>
 module.exports = {
   key: '<%= KEY %>',
   noun: '<%= NOUN %>',
@@ -897,7 +988,8 @@ module.exports = {
       getInputFields<% } %>
     ],
     outputFields: [
-<%= SAMPLE %>
+<%= SAMPLE %><% if (hasCustomOutputFields) { %><% if (SAMPLE) { %>,<% } %>
+      getOutputFields<% } %>
     ],
     perform: getList
   }
